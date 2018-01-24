13bbfe650fca438bfef1e813b5c6ef05c904143d
Matthew Richmond <mjr1408@rit.edu>
2018-01-24 18:08:03 -0500

    Currently sorts list and outputs it

--- src/ltools.java
+++ src/ltools.java
@@ -9,47 +9,58 @@ import java.util.Scanner;
  *
  */
 public class ltools {
-    private ArrayList<Integer> data = new ArrayList<>(); //Data to be stored and used by these tools
 
     /**
      * helper function to populate data, reads from stdin.
      * Handles type-checking, as it terminates when it recieves
      * something that isn't an Int
      */
-    private void lpop(){
+    private static void lpop(ArrayList<Integer> data){
         Scanner in = new Scanner(System.in);
-        while(in.hasNextInt()){
-            int x = in.nextInt();
-            data.add(x);
+        System.out.print(">");
+        String x;
+        int z;
+        while(!(x = in.nextLine()).equals("")){
+            System.out.println(x);
+            z = Integer.valueOf(x);
+            System.out.print(">");
+            data.add(z);
         }
+        in.close();
     }
 
     /**
      * Sorts data using an insertion sort algorithm
      * since isort is an in-place sort, no need for a return type
-     * @param data the arraylist containing the data that needs sorted
+     * @param arr the arraylist containing the data that needs sorted
      */
-    private void isort(ArrayList<Integer> data){
-        int i = 0;
-        while (i < data.size()){
-            int x = data.get(i);
-            int j = i - 1;
-            while (j >= 0 && data.get(j) > x){
-                data.add(j+1, data.remove(j));
-                j--;
+    private static void isort(ArrayList<Integer> arr)
+    {
+        int i, key, j;
+        for (i = 1; i < arr.size(); i++)
+        {
+            key = arr.get(i);
+            j = i-1;
+
+       /* Move elements of arr[0..i-1], that are
+          greater than key, to one position ahead
+          of their current position */
+            while (j >= 0 && arr.get(j) > key)
+            {
+                arr.add(j+1, arr.remove(j));
+                j = j-1;
             }
-            data.add(j+1,x);
-            i++;
         }
     }
 
-    public void main(String[] args){
-        if (args.length == 2){
+    public static void main(String[] args){
+        if (args.length == 1){
             System.out.println("Reading csvs from file: " + args[1]);
         }
-        else if(args.length == 1){
+        else if(args.length == 0){
             System.out.println("Please enter your data, one number at a time, enter a non-integer when done");
-            lpop();
+            ArrayList<Integer> data = new ArrayList<>();
+            lpop(data);
             isort(data);
             System.out.println(data);
         }
