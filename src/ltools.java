a146ab1b49d3ead68f4672966fe98f865a2324a9
Matthew Richmond <mjr1408@rit.edu>
2018-01-29 11:36:07 -0500

    Working on more in-depth functionality

--- src/ltools.java
+++ src/ltools.java
@@ -1,6 +1,6 @@
-import java.io.File;
-import java.io.InputStream;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -21,7 +21,6 @@ public class ltools {
         String x;
         int z;
         while(!(x = in.nextLine()).equals("")){
-            System.out.println(x);
             z = Integer.valueOf(x);
             System.out.print(">");
             data.add(z);
@@ -41,10 +40,6 @@ public class ltools {
         {
             key = arr.get(i);
             j = i-1;
-
-       /* Move elements of arr[0..i-1], that are
-          greater than key, to one position ahead
-          of their current position */
             while (j >= 0 && arr.get(j) > key)
             {
                 arr.add(j+1, arr.remove(j));
@@ -53,19 +48,87 @@ public class ltools {
         }
     }
 
-    public static void main(String[] args){
+    private static int mean(ArrayList<Integer> data){
+        int ttl = 0;
+        int count = 0;
+        for(int x: data){
+            ttl += x;
+            count++;
+        }
+        return ttl/count;
+    }
+
+    private static int mode(ArrayList<Integer> data){
+        HashMap<Integer, Integer> numlist = new HashMap<>();
+        for(int x: data){
+            if(numlist.containsKey(x)){
+                numlist.put(x, numlist.get(x) + 1);
+            }
+            else{
+                numlist.put(x, 1);
+            }
+        }
+        for(int z: numlist.keySet()){
+            int most = 0;
+            if(numlist.get(z) > most){
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException{
+        ArrayList<Integer> data = new ArrayList<>();
         if (args.length == 1){
-            System.out.println("Reading csvs from file: " + args[1]);
+            System.out.println("Reading csv's from file: " + args[0]);
+            File f = new File(args[0]);
+            Scanner fin = new Scanner(f);
+            while(fin.hasNext()){
+                String input = fin.nextLine();
+                String[] csv = input.split(",");
+                for(String x: csv){
+                    data.add(Integer.valueOf(x));
+                }
+            }
+            isort(data);
         }
         else if(args.length == 0){
             System.out.println("Please enter your data, one number at a time, enter a non-integer when done");
-            ArrayList<Integer> data = new ArrayList<>();
             lpop(data);
             isort(data);
-            System.out.println(data);
         }
         else {
             System.out.println("useage: java ltools.java [fname:]");
+        }
+        System.out.println("List sorted.");
+        System.out.println("Options: [p]rint, [o]utput to file, [mo]de, [me]dian, [mea]n, [ou]tput to file with data");
+        System.out.println("Enter a blank line when finished");
+        Scanner in = new Scanner(System.in);
+        String x;
+        while(!( x =in.nextLine()).equals("")){
+            if(x.equals("p")){
+                System.out.println(data);
+            }
+            else if(x.equals("o")){
+                BufferedWriter out = new BufferedWriter(new FileWriter("output.txt"));
+                for(int d: data){
+                    out.write(d);
+                    out.write(",");
+                }
+                out.write("\b");
+                out.close();
+            }
+            else if(x.equals("mo")){
+
+            }
+            else if(x.equals("mea")){
+
+            }
+            else if(x.equals("ou")){
+
+            }
+            else{
+                System.out.println("Options: [p]rint, [o]utput to file, [mo]de, [me]dian, [mea]n," +
+                        " [ou]tput to file with data");
+
+            }
         }
     }
 }
